bad3dd60f2fbb52e1f80c090ef6a61d86c603f47
Ushastaya <88803074+Ushastaya@users.noreply.github.com>
2024-07-25 15:33:47 +0300

    ДЗ 3. Задание 1.

---
+++ Lessons3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задание 1
+void VvediNumberPokaNeVvedeshChetnoeOrQ()
+{
+    bool ext = false;
+    do
+    {
+        string input = Console.ReadLine();
+        if (GetNumberOrNot(input))
+        {
+            int writeNumber = Convert.ToInt32(input);
+            ext = GetEvenNumberOrNot(writeNumber);
+        }
+        else
+        {
+            if (input == "q")
+            {
+                ext = true;
+            }
+        }
+
+    } while (!ext);
+}
+
+bool GetNumberOrNot(string text)
+{
+    if (int.TryParse(text, out int number))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool GetEvenNumberOrNot(int number)
+{
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
